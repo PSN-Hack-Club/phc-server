5bf8c6b6961ab43e038ef7b8b04718a97bb8058e
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-02-04 20:37:18 +0530

    add discord schema

--- models/invite.model.js
+++ models/invite.model.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose')
+const {Schema, model, models} = require('mongoose')
 
-const inviteModel = new mongoose.Schema({
+const inviteModel = new Schema({
     email: {
-        type: String,
+        type: Schema.Types.String,
         required: true,
     },
     name: {
-        type: String,
+        type: Schema.Types.String,
         required: true,
     },
     inviteUrl: {
-        type: String,
+        type: Schema.Types.String,
         required: true,
     },
+    discordId: {
+        type: Schema.Types.String,
+        requires: false
+    }
 })
 
-module.exports = mongoose.models.Invite || mongoose.model('Invite', inviteModel)
+module.exports = models.Invite || model('Invite', inviteModel)
