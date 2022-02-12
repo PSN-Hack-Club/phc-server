09167b3d5603c2371a20cfa5bac99b9a917084cf
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-02-12 16:28:54 +0530

    gmail moment gay

--- utils/emailClient.js
+++ utils/emailClient.js
@@ -2,52 +2,42 @@ const nodemailer = require('nodemailer')
 const {google} = require('googleapis')
 const OAuth2 = google.auth.OAuth2;
 
-const createTransporter = () => nodemailer.createTransport({
-    port: 465,
-    host: 'smtp.gmail.com',
-    auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD,
-    },
-    secure: true,
-})
+const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+        process.env.EMAIL_CLIENT_ID,
+        process.env.EMAIL_CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+    );
 
-// const createTransporter = async () => {
-//     const oauth2Client = new OAuth2(
-//         process.env.EMAIL_CLIENT_ID,
-//         process.env.EMAIL_CLIENT_SECRET,
-//         "https://developers.google.com/oauthplayground"
-//     );
-//
-//     oauth2Client.setCredentials({
-//         refresh_token: process.env.EMAIL_REFRESH_TOKEN
-//     });
-//
-//     let accessToken;
-//     try {
-//         accessToken = await new Promise((resolve, reject) => {
-//             oauth2Client.getAccessToken((err, token) => {
-//                 if (err) {
-//                     reject("Failed to create access token :(");
-//                 }
-//                 resolve(token);
-//             });
-//         });
-//     }
-//     catch (e) {
-//         console.log(e)
-//         return
-//     }
-//
-//     return nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//             type: "OAuth2",
-//             user: process.env.EMAIL_ADDRESS,
-//             accessToken,
-//         }
-//     });
-// };
+    oauth2Client.setCredentials({
+        refresh_token: process.env.EMAIL_REFRESH_TOKEN
+    });
+
+    let accessToken;
+    try {
+        accessToken = await new Promise((resolve, reject) => {
+            oauth2Client.getAccessToken((err, token) => {
+                if (err) {
+                    reject("Failed to create access token :(");
+                }
+                resolve(token);
+            });
+        });
+    }
+    catch (e) {
+        console.log(e)
+        return
+    }
+
+    return nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL_ADDRESS,
+            accessToken,
+        }
+    });
+};
 
 const sendEmail = async (emailOptions) => {
     const emailTransporter = createTransporter();
