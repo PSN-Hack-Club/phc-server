3712664b99c0aa832dac0364ddb99ce6c1d71fb6
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-03-01 12:19:15 +0530

    trying something

--- index.js
+++ index.js
@@ -3,114 +3,131 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Invites = require("./models/invite.model");
-const {validPathwaysEmail, nameFromEmail} = require("./utils/emails");
-const sendEmail = require("./utils/emailClient");
+const { validPathwaysEmail, nameFromEmail } = require("./utils/emails");
+const { sendEmail } = require("./utils/emailClient");
+const basicAuth = require("express-basic-auth");
 const axios = require("axios");
 const PORT = 1337;
 
 mongoose.connect(process.env.MONGODB_URL).catch((err) => {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 });
 
 const app = express();
 app.use(express.json());
 
 app.use(
-    cors({
-        origin: (process.env.NODE_ENV || "development") === "development" ? "http://localhost:3000" : "https://psn.hackclub.com",
-        credentials: true,
-    })
+  cors({
+    origin:
+      (process.env.NODE_ENV || "development") === "development"
+        ? "http://localhost:3000"
+        : "https://psn.hackclub.com",
+    credentials: true,
+  })
 );
 
-app.post('/join', async (req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(422).send()
-    }
-    const email = req.body.email
-    if (!email) {
-        return res.status(400).send({msg: 'No email provided'})
-    }
+app.post("/join", async (req, res) => {
+  const email = req.body.email;
+  if (!email) {
+    return res.status(400).send({ msg: "No email provided" });
+  }
 
-    const isValid = validPathwaysEmail(email)
-    if (!isValid) {
-        return res.status(400).send({msg: 'Invalid Email Provided'})
-    }
+  const isValid = validPathwaysEmail(email);
+  if (!isValid) {
+    return res.status(400).send({ msg: "Invalid Email Provided" });
+  }
 
+  try {
+    const invite = await Invites.findOne({ email });
+    if (invite) {
+      return res.status(409).send({ msg: "Invite already sent" });
+    }
+  } catch {}
+
+  const name = nameFromEmail(email);
+
+  let resp;
+
+  const token = process.env.DISCORD_TOKEN;
+  try {
+    const channel = process.env.DISCORD_CHANNEL;
+    resp = await axios.post(
+      `https://discordapp.com/api/v6/channels/${channel}/invites`,
+      {
+        max_age: 172800, // 48 hours in seconds
+        max_uses: 2,
+        unique: true,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bot ${token}`,
+        },
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    console.log("Discord issue");
+    return res
+      .status(500)
+      .send({ msg: "Internal Server Error\nPlease try again later!" });
+  }
+
+  const { code } = resp.data;
+  const url = `https://discord.gg/${code}`;
+
+  try {
+    await sendEmail({
+      from: process.env.EMAIL_ADDRESS,
+      to: email,
+      subject: "PSN Hack Club - Discord Invite",
+      name,
+      header: "Welcome to the club!",
+      content: [
+        `You're receiving this email because your email was used to sign up for the PSN Hack Club!<br/>Join the discord server by clicking <a href="${url}" target="_blank">this link</a>. The invite will expire in 48 hours.`,
+        `If that did not work, please use the link below.<br/><a href="${url}">${url}</a>`,
+        `You can ignore this email if you did not request an invite.`,
+      ],
+    });
+  } catch (e) {
+    console.log(e);
     try {
-        const invite = await Invites.findOne({email})
-        if (invite) {
-            return res.status(409).send({msg: 'Invite already sent'})
-        }
-    } catch {
-    }
+      await axios.delete(`https://discordapp.com/api/v6/invtes/${code}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bot ${token}`,
+        },
+      });
+    } catch {}
+    return res
+      .status(500)
+      .send({ msg: "Internal Server Error\nPlease try again later!" });
+  }
 
-    const name = nameFromEmail(email)
+  try {
+    await Invites.create({ email, name, inviteUrl: url });
+  } catch {}
 
-    let resp
+  return res.status(200).send({ msg: "Email sent!" });
+});
 
-    const token = process.env.DISCORD_TOKEN
-    try {
-        const channel = process.env.DISCORD_CHANNEL
-        resp = await axios.post(
-            `https://discordapp.com/api/v6/channels/${channel}/invites`,
-            {
-                max_age: 172800, // 48 hours in seconds
-                max_uses: 2,
-                unique: true,
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bot ${token}`,
-                },
-            }
-        )
-    } catch (e) {
-        console.error(e)
-        console.log('Discord issue')
-        return res.status(500).send({msg: 'Internal Server Error\nPlease try again later!'})
-    }
+app.get("/", (req, res) => {
+  res.status(200).send("pong");
+});
 
-    const {code} = resp.data
-    const url = `https://discord.gg/${code}`
-
-    try {
-        await sendEmail({
-            from: process.env.EMAIL_ADDRESS,
-            to: email,
-            subject: 'PSN Hack Club - Discord Invite',
-            html: `<div style="width: 100%; border-radius: 1em; background-color: white !important; color: black;"><p style="font-size: 1.5rem; margin-bottom:0; font-weight: 800">Welcome to the club!</p><p>Dear ${name},</p><p>You're receiving this email because your email was used to sign up for the PSN Hack Club!<br/>Join the discord server by clicking <a href="${url}" target="_blank">this link</a>. The invite will expire in 48 hours.</p><p>If that did not work, please use the link below.<br/><a href="${url}">${url}</a></p><p>You can ignore this email if you did not request an invite.</p><p>Best Regards<br/>PSN Hack Club</p><p style="font-size: 0.65rem; color: gray;">This is a one time email, you have not been subscribed to any service or newsletter.<br/>PSN Hack Club, Pathways School Noida, Noida, Uttar Pradesh<p/></p></div>`,
-        })
-    } catch (e) {
-        console.log(e)
-        try {
-            await axios.delete(
-                `https://discordapp.com/api/v6/invtes/${code}`,
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bot ${token}`,
-                    },
-                }
-            )
-        } catch {
-        }
-        return res.status(500).send({msg: 'Internal Server Error\nPlease try again later!'})
-    }
-
-    try {
-        await Invites.create({email, name, inviteUrl: url})
-    } catch {
-    }
-
-    return res.status(200).send({msg: 'Email sent!'})
-})
-
-app.get('/', (req, res) => {
-    res.status(200).send('pong')
-})
+app.post(
+  "/sendToAll",
+  basicAuth({
+    users: {
+      admin: process.env.BASIC_AUTH,
+    },
+  }),
+  async (req, res) => {
+    const invites = await Invites.find({}).select("email").exec();
+  }
+);
 
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
+  console.log(`server started on port ${PORT}`);
 });
--- utils/emailClient.js
+++ utils/emailClient.js
@@ -1,54 +1,66 @@
-const nodemailer = require('nodemailer')
-const {google} = require('googleapis')
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
+const Handlebars = require("handlebars");
+const fs = require("fs");
 
 const createTransporter = async () => {
-    const oauth2Client = new OAuth2(
-        process.env.EMAIL_CLIENT_ID,
-        process.env.EMAIL_CLIENT_SECRET,
-        "https://developers.google.com/oauthplayground"
-    );
+  const oauth2Client = new OAuth2(
+    process.env.EMAIL_CLIENT_ID,
+    process.env.EMAIL_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
 
-    oauth2Client.setCredentials({
-        refresh_token: process.env.EMAIL_REFRESH_TOKEN
-    });
+  oauth2Client.setCredentials({
+    refresh_token: process.env.EMAIL_REFRESH_TOKEN,
+  });
 
-    let accessToken;
-    try {
-        accessToken = await oauth2Client.getAccessToken();
-    } catch (e) {
-        console.log(e)
-        return
-    }
+  let accessToken;
+  try {
+    accessToken = await oauth2Client.getAccessToken();
+  } catch (e) {
+    console.log(e);
+    return;
+  }
 
-    return nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: process.env.EMAIL_ADDRESS,
-            clientId: process.env.EMAIL_CLIENT_ID,
-            clientSecret: process.env.EMAIL_CLIENT_SECRET,
-            refreshToken: process.env.EMAIL_REFRESH_TOKEN,
-            accessToken: accessToken,
-        }
-    });
+  return nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_ADDRESS,
+      clientId: process.env.EMAIL_CLIENT_ID,
+      clientSecret: process.env.EMAIL_CLIENT_SECRET,
+      refreshToken: process.env.EMAIL_REFRESH_TOKEN,
+      accessToken: accessToken,
+    },
+  });
 };
 
-const sendEmail = async (emailOptions) => {
-    const emailTransporter = await createTransporter();
-    if (!emailTransporter) {
-        throw "Could not make email transporter"
-    }
-    await new Promise((resolve, reject) => {
-        emailTransporter.sendMail(emailOptions, (err, info) => {
-            if (err) {
-                reject(err)
-            }
-            resolve()
-        });
-    })
+const sendEmailRaw = async (emailOptions) => {
+  const emailTransporter = await createTransporter();
+  if (!emailTransporter) {
+    throw "Could not make email transporter";
+  }
+  await new Promise((resolve, reject) => {
+    emailTransporter.sendMail(emailOptions, (err, info) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
 
-    emailTransporter.close()
+  emailTransporter.close();
 };
 
-module.exports = sendEmail
+const sendEmail = async ({ header, name, content, ...emailOptions }) => {
+  await sendEmailRaw({
+    ...emailOptions,
+    html: Handlebars.compile(
+      fs.readFileSync("../templates/email.hbs", "utf8"),
+      { header, name, content }
+    ),
+  });
+};
+
+module.exports = { sendEmail, sendEmailRaw };
