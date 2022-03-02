7a7c7dd642b6bd1ed3f93b180db27189c5e852b4
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-03-02 11:52:40 +0530

    fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 const connectDb = require('../middleware/mongodb')
-const Invites = require('./models/invite.model')
-const { utcTimeNow } = require('./utils/time')
-
-router.use(connectDb)
+const Invites = require('../models/invite.model')
+const { utcTimeNow } = require('../utils/time')
 
 router.post('/sendToAll', async (req, res) => {
   const invites = await Invites.find({}).select('email').exec()
@@ -14,17 +12,20 @@ router.post('/sanitize', async (req, res) => {
   const expiryTime = timeNow - 172800000
 
   // Delete all invites that have expired and don't have an ID yet
-  const deleted = await Invites.deleteMany({
-    $or: [
-      {
-        createdAt: { $lte: expiryTime },
-      },
-      { createdAt: { $exists: false } },
-    ],
-    discordId: { $exists: false },
-  })
-
-  res.status(201).send(deleted)
+  try {
+    const deleted = await Invites.deleteMany({
+      $or: [
+        {
+          createdAt: { $lte: expiryTime },
+        },
+        { createdAt: { $exists: false } },
+      ],
+      discordId: { $exists: false },
+    })
+    res.status(201).send(deleted)
+  } catch (e) {
+    res.status(500).send({ error: e })
+  }
 })
 
 module.exports = router
